f29cfad24b777956abccf8aee64520daa3d8fdc9
Felix/failaxite <failaxite.don@gmail.com>
2024-11-29 16:53:59 +0100

    :bug: Fixed Deposit controller errors

--- src/Controller/DepotController.php
+++ src/Controller/DepotController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Deposit;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Form\DepotType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DepotController extends AbstractController
 {
